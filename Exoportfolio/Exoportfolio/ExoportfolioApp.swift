ac72b174f5357cd0f453fc94003a295e9958b159
Mathieu Durand <32414550+MathieuDurand28@users.noreply.github.com>
2023-11-24 12:05:21 +0100

    first

---
+++ Exoportfolio/Exoportfolio/ExoportfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExoportfolioApp.swift
+//  Exoportfolio
+//
+//  Created by Mathieu Durand on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExoportfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
